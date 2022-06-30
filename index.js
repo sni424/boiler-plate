da8a55583d44857a845e41d9ea18c8f3a3076035
Your Name <you@example.com>
2022-06-30 22:21:18 +0900

    확인

--- index.js
+++ index.js
@@ -51,8 +51,6 @@ app.post('/api/users/login',(req,res)=>{
     }
      //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
      user.comparePassword(req.body.password, (err, isMatch) => {
-        // console.log('err',err)
-        // console.log('isMatch',isMatch)
         if (!isMatch)
           return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." }) 
         //비밀번호 까지 맞다면 토큰을 생성하기.
