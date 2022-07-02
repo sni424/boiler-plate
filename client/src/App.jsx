8cd29eeee766a5c5af61fbe00c97885912749b21
sni424 <jojonghyeok424@gamil.com>
2022-07-02 20:34:10 +0900

    node.js 와 redux로 회원가입 및 로그인

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import LadingPage from './components/views/LandingPage';
 import LoginPage from './components/views/LoginPage';
 import RegisterPage from './components/views/RegisterPage/idex';
+import Auth from './hoc/auth';
 
 const App = () => {
+  const navi = useNavigate();
+  const NewLandingPage = Auth(LadingPage, null);
+
+  const NewLoginPage = Auth(LoginPage, false);
+
+  const NewRegisterPage = Auth(RegisterPage, false);
+
   return (
     <Routes>
-      <Route path="/" element={<LadingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/" element={<NewLandingPage navi={navi} />} />
+      <Route path="/login" element={<NewLoginPage navi={navi} />} />
+      <Route path="/register" element={<NewRegisterPage navi={navi} />} />
     </Routes>
   );
 };
--- client/src/components/views/LoginPage/index.jsx
+++ client/src/components/views/LoginPage/index.jsx
@@ -1,46 +1,61 @@
 import axios, { Axios } from 'axios';
-import { useState } from 'react'
-import {useDispatch} from `react-redux`;
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
 
 const LoginPage = () => {
   const dispatch = useDispatch();
+  const navi = useNavigate();
 
-  const [Email,setEmail] = useState("");
-  const [Password,setPassword] = useState("");
+  const [Email, setEmail] = useState('');
+  const [Password, setPassword] = useState('');
 
-  const onSubmitHandler = (e)=>{
-  e.preventDefault();
-  let body = {email: Email,password: Password}
-  // dispatch(loginUser(body))
-Axios.post('/api/users/login',body).then(res=>{
-    res.data
-  })
-  }
-
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    let body = { email: Email, password: Password };
+    dispatch(loginUser(body)).then((res) => {
+      if (res.payload.loginSuccess) {
+        navi('/');
+      } else {
+        alert('오류');
+      }
+    });
+  };
 
   return (
-    <div style={{display:"flex", justifyContent:'center',alignItems:"center"
-    ,width:"100%",height:"100vh"
-    }} >
-     <form style={{display:"flex",flexDirection:"column"}}
-     onSubmit={onSubmitHandler}
-     >
-      <label>Email</label>
-      <input type="email" value={Email} onChange={(e)=>{
-setEmail(e.target.value)
-      }} />
-      <label>Password</label>
-      <input type="password" value={Password} onChange={
-        (e)=>{
-          setPassword(e.target.value);
-        }
-      } />
-      <button style={{marginTop:"1rem"}}>
-        Login
-      </button>
-     </form>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={onSubmitHandler}>
+        <label>Email</label>
+        <input
+          type="email"
+          value={Email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          value={Password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button type="submit" style={{ marginTop: '1rem' }}>
+          Login
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
