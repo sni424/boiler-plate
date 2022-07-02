531a21896f587954b9d11f3804f6760f6251fd04
sni424 <jojonghyeok424@gamil.com>
2022-07-02 18:02:35 +0900

    concurrently설정 및 prettier

--- client/src/components/views/LandingPage/index.jsx
+++ client/src/components/views/LandingPage/index.jsx
@@ -1,19 +1,24 @@
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 import axios from 'axios';
 
 const LadingPage = () => {
-
-  useEffect(()=>{
-    axios.get("/api/hello").then(res=>console.log(res.data))
-  })
+  useEffect(() => {
+    axios.get('/api/hello').then((res) => console.log(res.data));
+  });
 
   return (
-    <div style={{display:"flex", justifyContent:'center',alignItems:"center"
-    ,width:"100%",height:"100vh"
-    }} >
-     <h2>시작 페이지</h2>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <h2>시작 페이지</h2>
     </div>
-  )
-}
+  );
+};
 
-export default LadingPage
+export default LadingPage;
